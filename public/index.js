08766728cc239c2cf06b58e3d821865b38e2f429
vickypython <vickymlucky6@gmail.com>
2024-06-03 09:33:32 +0300

    first commit for cafe leu

---
+++ public/index.js
@@ -0,0 +1,9 @@
+const burger = document.getElementById("burger");
+const menu = document.getElementById("menu");
+burger.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
