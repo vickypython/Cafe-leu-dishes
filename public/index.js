6a5250543cb2134c09a799656ef5e766f51ab94a
vickypython <vickymlucky6@gmail.com>
2024-06-06 21:39:21 +0300

    added testimonial section

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
 const burger = document.getElementById("burger");
 const menu = document.getElementById("menu");
+const fragement=document.createDocumentFragment()
 burger.addEventListener("click", () => {
   if (menu.classList.contains("hidden")) {
     menu.classList.remove("hidden");
   } else {
     menu.classList.add("hidden");
   }
+
+
 });
