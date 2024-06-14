9c509d7c753af8d72167441c5d2fe328c240e3e6
vickypython <vickymlucky6@gmail.com>
2024-06-14 21:34:15 +0300

    contact us section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./public/**/*.{html,js}"],
+    content: ["./public/**/*.{html,js}",
+    "./node_modules/tw-elements/js/**/*.js"
+
+    ],
     theme: {
       extend: {},
     },
-    plugins: [],
+    plugins: [require('@tailwindcss/forms')({
+      strategy: 'base', // only generate global styles
+      strategy: 'class', // only generate classes
+    })],
+    darkmode:"class"
   }
+ 
